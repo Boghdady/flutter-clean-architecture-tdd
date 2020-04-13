a7974f9210904848c12516ff336993a0f4862c98
Ahmed Boghdady <boghdady@Ahmeds-iMac.local>
2020-04-13 08:25:21 +0200

    Working on LocalDataSource Implementation and Finished getLastCachedTriviaNumber

--- lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
+++ lib/features/number_trivia/data/datasources/number_trivia_local_data_source.dart
@@ -1,4 +1,10 @@
-import 'package:clean_architecture_tdd/features/number_trivia/data/models/number_trivia_model.dart';
+import 'dart:convert';
+
+import 'package:clean_architecture_tdd/core/error/exceptions.dart';
+import 'package:meta/meta.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../models/number_trivia_model.dart';
 
 abstract class NumberTriviaLocalDataSource {
   /// Gets the cached [NumberTriviaModel] which was gotten the last time
@@ -8,4 +14,32 @@ abstract class NumberTriviaLocalDataSource {
   Future<NumberTriviaModel> getLastNumberTrivia();
 
   Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache);
+}
+
+const CACHED_NUMBER_TRIVIA = 'CACHED_NUMBER_TRIVIA';
+
+class NumberTriviaLocalDataSourceImpl implements NumberTriviaLocalDataSource {
+  final SharedPreferences sharedPreferences;
+
+  NumberTriviaLocalDataSourceImpl({@required this.sharedPreferences});
+
+  @override
+  Future<NumberTriviaModel> getLastNumberTrivia() {
+    // Storing complex objects such as NumberTrivia inside shared preferences is possible only in a string format
+    final numberTriviaAsString =
+        sharedPreferences.getString(CACHED_NUMBER_TRIVIA);
+
+    if (numberTriviaAsString != null) {
+      final numberTriviaModel =
+          NumberTriviaModel.fromJson(json.decode(numberTriviaAsString));
+      return Future.value(numberTriviaModel);
+    } else {
+      throw CacheException();
+    }
+  }
+
+  @override
+  Future<void> cacheNumberTrivia(NumberTriviaModel triviaToCache) {
+    return null;
+  }
 }
